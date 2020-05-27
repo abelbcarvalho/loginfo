0e290e08cd749acb587ec18599ebed15c714d9d4
abelbcarvalho <abelbdecarvalho@gmail.com>
2020-05-27 14:18:12 -0300

    Associated Tables Model create and added to the Project

---
+++ src/br/com/bcarsoft/accountdate/model/AccountDate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bcarsoft.accountdate.model;
+
+import br.com.bcarsoft.structure.model.AbsModel;
+
+/**
+ * Model for associated Table Model - Account and Date.
+ * @author abel
+ */
+public class AccountDate extends AbsModel {
+
+    /**
+     * Builder Method.
+     */
+    public AccountDate() {
+        super();
+    }
+    
+}
---
+++ src/br/com/bcarsoft/sitedate/model/SiteDate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bcarsoft.sitedate.model;
+
+import br.com.bcarsoft.structure.model.AbsModel;
+
+/**
+ * Model for associated Table Model - Site and Date.
+ * @author abel
+ */
+public class SiteDate extends AbsModel {
+
+    /**
+     * Builder Method.
+     */
+    public SiteDate() {
+        super();
+    }
+    
+}
